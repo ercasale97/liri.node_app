d95631dd38e1e8a4cf402f5110d06625737bb20f
ercasale97 <ercasale97@gmail.com>
2019-02-27 22:10:05 -0600

    it finally works

--- liri.js
+++ liri.js
@@ -1,95 +1,176 @@
 require("dotenv").config();
-var keys = require('./javascript/keys');
+var axios = require("axios");
+var moment = require("moment");
+moment().format();
+var fs = require("fs");
+
+var keys = require("./keys.js");
+var request = require("request")
 var Spotify = require('node-spotify-api');
-var request = require('request');
-var moment = require('moment');
+var spotify = new Spotify(keys.spotify);
+
+var query = process.argv;
+var type = process.argv[2];
+var array = [];
+
+//Loop through and join name of arguments after file name
+for (var i = 3; i < query.length; i++) {
+    array.push(query[i]);
+    array.push("+")
+}
+
+array.splice(-1); //Get rid of last plus sign, if left errors caused
+var finalSearch = array.join(""); //Search query joined together to form string for any query below
+
+//Switch statement to determine type selected. (Ex. concert-this, movie-this, etc.)
+switch (type) {
+    case 'concert-this':
+        concertMe()
+        break;
+    case 'spotify-this-song':
+        spotifyIt()
+        break;
+    case 'movie-this':
+        movieThis()
+        break;
+    case 'do-what-it-says':
+        itSays()
+        break;
+    default:
+        console.log("No value available");
+}
 
 
-var spotify = new Spotify({
-    id: keys.spotify.id,
-    secret: keys.spotify.secret
+// node liri.js concert-this
+
+function concertMe() {
+    if (finalSearch === "") {
+        console.log('\n')
+        console.log("No Artist entered. Please enter an Artist")
+        console.log('\n')
+    } else {
+        axios.get("https://rest.bandsintown.com/artists/" + finalSearch + "/events?app_id=codingbootcamp").then(
+        function (response) {
+           if(response.data.length <= 0) {
+               console.log("No info for this Artist")
+           }else {
+            for(var i=0; i < response.data.length; i++) {
+
+                var currData = `\n
+    Venue: ${response.data[i].venue.name}
+    Location: ${response.data[i].venue.city + ", " + response.data[0].venue.region}
+    Event Date: ${moment(response.data[i].datetime).format('LL')}
+            `
+            console.log(currData)
+            }
+           }
+           
+            dataLog(currData)
+        }
+    );
+    }
+}
+
+// node liri.js spotify-this-song
+
+function spotifyIt() {
+
+    if (finalSearch === "") {
+        finalSearch = "ace+of+base+the+sign"
+    }
+
+    spotify.search({
+        type: 'artist,track',
+        query: finalSearch
+    }, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log('\n')
+
+        var currData = `\n
+    Artist: ${data.tracks.items[0].artists[0].name}
+    Track: ${data.tracks.items[0].name}
+    Preview: ${data.tracks.items[0].preview_url}
+    Album: ${data.tracks.items[0].album.name}
+            `
+            console.log(currData)
+            dataLog(currData)
+
+    });
+}
+
+// node liri.js movie-this
+
+function movieThis() {
+
+    if (finalSearch === "") {
+        finalSearch = "mr+nobody"
+    }
+
+    axios.get("http://www.omdbapi.com/?t=" + finalSearch + "&y=&plot=short&apikey=trilogy").then(
+        function (response) {
+        
+            var currData = `\n
+    Title: ${response.data.Title}
+    Released: ${response.data.Year}
+    IMDB Rating: ${response.data.imdbRating}
+    Rotten Tomatos Rating: ${response.data.Ratings[1].Value}
+    Country: ${response.data.Country}
+    Language: ${response.data.Language}
+    Plot: ${response.data.Plot}
+    Actors: ${response.data.Actors}
+            `
+            console.log(currData)
+            dataLog(currData)
+        }
+    );
+
+    
+}
+
+// node liri.js do-what-it-says
+
+function itSays() {
+    fs.readFile("random.txt", "utf8", function(error, data) {
+
+        if (error) {
+          return console.log(error);
+        }
+
+        var dataArr = data.split(",");
+      
+        finalSearch = dataArr[1];
+        spotifyIt()
+      });
+}
+
+//Input Logger - see log.txt
+
+var logQuery = query.splice(0,2)
+logQuery =  "\n" + query.join(" ") + "\n"
+console.log(logQuery)
+
+fs.appendFile("log.txt", logQuery, function(err) {
+
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Log Updated");
+    }
+  
   });
 
+//Data Logger - see log.txt
 
-if (process.argv[2] == 'concert-this' ) {
-   
-    var artist = process.argv.slice(3).join(" ")
-    console.log(artist);
-   
-    var queryURL = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp";
+function dataLog(data) {
+    fs.appendFile("log.txt", data, function(err) {
 
-    request(queryURL, function (error, response, body) {
-        if (error) console.log(error);
-        var result  =  JSON.parse(body)[0];
-        console.log("Venue name " + result.venue.name);
-        console.log("Venue location " + result.venue.city);
-        console.log("Date of Event " +  moment(result.datetime).format("MM/DD/YYYY"));
-       
-
-
-    });
-
-
-    // Name of the venue
-    // Venue location
-    // Date of the Event (use moment to format this as "MM/DD/YYYY")   
-} else if ( process.argv[2] == 'spotify-this-song') {
-
-    var songName = process.argv.slice(3).join(" ");
-
-    if (songName == undefined) {
-        songName = "The sign by Ace of Base";
-    } 
-   
-
-     spotify.search({ type: 'track', query: songName, limit: 10  }, function(err, data) {
-            if (err) {
-            return console.log('Error occurred: ' + err);
-            }
-
-            var tableArray = [];
-
-            for (var i = 0; i < data.tracks.items.length; i++ ) {
-                var result = {
-                    artist : data.tracks.items[i].album.artists[0].name,
-                    album_name : data.tracks.items[i].album.name,
-                    song_name : data.tracks.items[i].name,
-                    preview_url : data.tracks.items[i].preview_url 
-                }
-                tableArray.push(result);
-            }
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Log Updated");
+        }
       
-            
-            var table = cTable.getTable(tableArray);
-    
-            console.log(table);
-
-       
-    });
-
-
-// Default to "The Sign" by Ace of Base if no song provided
-} else if ( process.argv[2] == 'movie-this') {
-    var movieName = process.argv.slice(3).join(" ");
-
-    if (movieName == undefined) {
-        movieName = "Mr. Nobody";
-    } 
-
-    request('http://www.omdbapi.com/?i=tt3896198&apikey=4330589' + process.argv[3], function (error, response, body) {
-        
-        var result  =  JSON.parse(body);
-        console.log("Title :" + result.Title);
-        console.log("Year :" + result.Released);
-        console.log("IMDB Rating :" + result.imdbRating );
-        console.log("Rotten Tomatoes :" + result.Ratings[1].Value);
-        console.log("Country :" +  result.Country);
-        console.log("Language :" + result.Language);
-        console.log("Movie Plot :" + result.Plot);
-        console.log("Actors :" +  result.Actors);
-
-    });
-
-} else if ( process.argv[2] == 'do-what-it-says') {
-    console.log('do what it says')
-}
+      });
+  }
